0e144af2790c6226c07268cfad1f56525ca98952
jasekthewise <dantestargio@gmail.com>
2022-07-18 16:07:20 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// node packages
+const express = require('express')
+const fs = require('fs')
+const { join } = require('path')
+const { uid } = require('uid')
+// connections
+const app = express()
+let notes = require('./db/db.json')
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(join(__dirname, 'public')))
+// paths
+app.get('/', (req, res) => res.sendFile(join(__dirname, 'public', 'index.html')))
+app.get('/notes', (req, res) => res.sendFile(join(__dirname, 'public', 'notes.html')))
+app.get('/api/notes', (req, res) => res.json(notes))
+app.post('/api/notes', (req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uid(),
+    }
+    notes.push(newNote)
+    fs.writeFile(join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
+        if (err) {console.log(err)}
+        res.json(newNote)
+    })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    notes = notes.filter(note => note.id != req.params.id)
+    fs.writeFile(join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
+        if (err) {console.log(err)}
+        res.sendStatus(200)
+    })
+})
+// port
+app.listen(process.env.PORT || 3001)
